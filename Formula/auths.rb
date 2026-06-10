ee3298de65b72d55c29698cf59f22008d75af31a
bordumb <bordumbb@gmail.com>
2026-06-10 01:01:33 +0100

    auths 0.1.1

--- Formula/auths.rb
+++ Formula/auths.rb
@@ -1,24 +1,24 @@
 class Auths < Formula
   desc "Cryptographic identity for developers — sign artifacts, replace API keys"
   homepage "https://auths.dev"
-  version "0.0.1-rc.11"
+  version "0.1.1"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/auths-dev/auths/releases/download/v#{version}/auths-macos-aarch64.tar.gz"
-      sha256 "6d79216c1208ccf3520b1127ddc3285b037e8b8602ba5306439a71c7ff9b2a69"
+      sha256 "f26d862d135d5ffe1825f3b70e72312a068c4070b69d44f96662dd8750e6ae3e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/auths-dev/auths/releases/download/v#{version}/auths-linux-x86_64.tar.gz"
-      sha256 "317b96f68d47047f4cde2704656f98355796724c11c859994718bdaac7fa079c"
+      sha256 "d11ff378ffdb85d881728468f8e6b02f3511c09cafb10298dbf37493883ee6fc"
     end
     on_arm do
       url "https://github.com/auths-dev/auths/releases/download/v#{version}/auths-linux-aarch64.tar.gz"
-      sha256 "6c80dad0b2a67112ef4785c09cf4e18122479d3db39bc2fce768b00cc763b002"
+      sha256 "bb8437d99993e64494265580e3f9b632d4c058a70390fd979a074cdbb43923ec"
     end
   end
 
